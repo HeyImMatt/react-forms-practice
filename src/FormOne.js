fa565f51f69b599e875b5f190906bf7e0dc68699
heyimmatt <mrmcfarland@gmail.com>
2020-08-01 11:10:47 -0700

    Add submitHandler function and add users data to state

--- src/FormOne.js
+++ src/FormOne.js
@@ -4,19 +4,26 @@ function FormOne() {
   const INITIAL_FORM_STATE = {
     username:'',
   }
-  const [formData, setFormData] = useState(INITIAL_FORM_STATE)
+  const [formData, setFormData] = useState(INITIAL_FORM_STATE);
+  const [users, setUsers] = useState([]);
 
   const changeHandler = (e) => {
-    const { name, value } = e.target;
+    const {name, value} = e.target;
     setFormData((fdata) => ({
       ...fdata,
       [name]: value,
     }));
   }
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setUsers((oldUsers) => [...oldUsers, formData])
+    setFormData(INITIAL_FORM_STATE)
+  }
+
   return (
     <>
-      <form>
+      <form onSubmit={submitHandler}>
         <label htmlFor="usernameInput">Username:</label>
         <input 
         type="text" 
